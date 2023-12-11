5a7d7ffe40f435df12f45622a1f76c0432b311c2
JimcostDev <rjak727@gmail.com>
2023-12-11 15:21:07 +0100

    funcion calcularTiempoTranscurrido() optimizada para que muestre yrs and mos

--- assets/scripts/timeCalculator.js
+++ assets/scripts/timeCalculator.js
@@ -9,13 +9,18 @@ export function calcularTiempoTranscurrido(fechaInicio, fechaFinal) {
     }
 
     const mesesTranscurridos = (fechaFinalObj.getFullYear() - fechaInicioObj.getFullYear()) * 12 + (fechaFinalObj.getMonth() - fechaInicioObj.getMonth());
+    const aniosTranscurridos = Math.floor(mesesTranscurridos / 12);
+    const mesesRestantes = mesesTranscurridos % 12;
 
     let tiempo;
-    if (mesesTranscurridos > 12) {
-        const aniosTranscurridos = Math.floor(mesesTranscurridos / 12);
-        tiempo = `${aniosTranscurridos} yr`;
+
+    if (aniosTranscurridos === 0) {
+        tiempo = `${mesesRestantes} mos.`;
     } else {
-        tiempo = `${mesesTranscurridos} mos.`;
+        tiempo = `${aniosTranscurridos} yrs.`;
+        if (mesesRestantes > 0) {
+            tiempo += ` and ${mesesRestantes} mos.`;
+        }
     }
 
     return tiempo;
