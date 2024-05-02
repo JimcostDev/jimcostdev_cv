a28d7b7aad63b7bc730b47bedd31bb73ef2e1ac0
JimcostDev <rjak727@gmail.com>
2024-05-02 10:44:49 +0200

    quite archivos de dev, solo dejo los de producción

--- assets/scripts/user.js
+++
@@ -1,7 +0,0 @@
-export async function actualizarUsuario(datosUsuario) {
-    //*********** DATOS DEL USUARIO ************** */
-    const nombreUsuarioElement = document.getElementById('nombre-usuario');
-    if (nombreUsuarioElement) {
-        nombreUsuarioElement.textContent = datosUsuario.full_name;
-    }
-}
